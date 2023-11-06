1a69bd369d87d9072cd91d4c6631b6dfd6f79517
HuyDuong <93182239+FeikVizeege@users.noreply.github.com>
2023-11-06 17:08:45 +0700

    Setup Clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
-import Image from 'next/image'
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <p>Hello Admin Dashboard</p>
+    <div className='p-4'>
+      <UserButton afterSignOutUrl='/' />
+    </div>
   )
 }
